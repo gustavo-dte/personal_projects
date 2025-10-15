290b295a575e213cd757e5ac3a64352009f13480
Gustavo Andres Garcia Anguiano <a01795493@tec.mx>
2025-10-14 19:39:07 -0500

    multiple_subs_timmer_func

--- tests/constants_test.py
+++ tests/constants_test.py
@@ -1,10 +1,12 @@
 """
 Test constants for the Service Bus replication function.
+
+This module contains constants used specifically for testing purposes.
 """
 
-# Connection strings (dummy values for local tests)
-TEST_PRIMARY_CONN = "Endpoint=sb://primary-test.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=mockkey"
-TEST_SECONDARY_CONN = "Endpoint=sb://secondary-test.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=mockkey"
+# Test connection strings (for testing purposes only)
+TEST_SERVICEBUS_CONNECTION_STRING = "Endpoint=sb://test.servicebus.windows.net/"
+TEST_SECONDARY_CONNECTION_STRING = "Endpoint=sb://secondary.servicebus.windows.net/"
 
 # Topics and subscriptions
 TEST_TOPICS = ["topic-a", "topic-b"]
@@ -15,4 +17,3 @@ TEST_REPLICATION_TYPE = "primary_to_secondary"
 TEST_RTO_MINUTES = 10
 TEST_DELTA_MINUTES = 2
 TEST_MAX_RETRY_ATTEMPTS = 3
-TEST_BASE_RETRY_DELAY = 1.0
